3ba719cfa0d1bfa8b2b2b4499917d13b5caa5950
Pedro2804 <pedro28hdez@hotmail.com>
2024-06-14 21:02:59 -0600

    Subiendo cambios del proyecto;

--- DevStagram/app/Http/Controllers/ImagenController.php
+++ DevStagram/app/Http/Controllers/ImagenController.php
@@ -3,14 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Intervention\Image\Laravel\Facades\Image;
 
 class ImagenController extends Controller
 {
     public function store(Request $request)
     {
         $imagen = $request->file('file');
+        $nombreImagen = Str::uuid() . "." . $imagen->extension();
 
-        return response()->json(['imagen' => $imagen->extension()]);
+        $imagenServidor = Image::make($imagen);
+
+        $imagenServidor->fit(1000,1000);
+
+        $imagenPath = public_path('uploads') . '/' . $nombreImagen;
+        $imagenServidor->save($imagenPath);
+
+        return response()->json(['imagen' => $nombreImagen]);
     }
 }
 
